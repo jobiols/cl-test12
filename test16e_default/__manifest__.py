22050333e5b61476fd5a6b756271d0592bb32521
Jorge Obiols <jorge.obiols@gmail.com>
2025-02-05 14:47:30 -0300

    [FIX] reorganizar repos

--- test16e_default/__manifest__.py
+++ test16e_default/__manifest__.py
@@ -41,15 +41,15 @@
     "git-repos": [
         "https://github.com/jobiols/cl-test.git -b 16.0e",
 
-        "https://github.com/ingadhoc/account-financial-tools.git sub_l10n-ar/account-financial-tools",
-        "https://github.com/ingadhoc/account-payment.git sub_l10n-ar/account-payment",
-        "https://github.com/ingadhoc/odoo-argentina.git sub_l10n-ar/odoo-argentina",
-        "https://github.com/ingadhoc/argentina-sale.git sub_l10n-ar/argentina-sale",
-        "https://github.com/ingadhoc/account-invoicing.git sub_l10n-ar/account-invoicing",
-        "https://github.com/ingadhoc/odoo-argentina-ee.git sub_l10n-ar/odoo-argentina-ee",
-        "https://github.com/ingadhoc/stock.git sub_l10n-ar/stock",
-        "https://github.com/ingadhoc/aeroo_reports.git sub_l10n-ar/aeroo_reports",
-        "https://github.com/ingadhoc/sale.git sub_l10n-ar/sale",
+        "https://github.com/ingadhoc/account-financial-tools.git adhoc-account-financial-tools",
+        "https://github.com/ingadhoc/account-payment.git adhoc-account-payment",
+        "https://github.com/ingadhoc/odoo-argentina.git adhoc-odoo-argentina",
+        "https://github.com/ingadhoc/argentina-sale.git adhoc-argentina-sale",
+        "https://github.com/ingadhoc/account-invoicing.git adhoc-account-invoicing",
+        "https://github.com/ingadhoc/odoo-argentina-ee.git adhoc-odoo-argentina-ee",
+        "https://github.com/ingadhoc/stock.git adhoc-stock",
+        "https://github.com/ingadhoc/aeroo_reports.git adhoc-aeroo_reports",
+        "https://github.com/ingadhoc/sale.git adhoc-sale",
 
     ],
     # list of images to use in the form 'name image-url'
