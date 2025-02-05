8f6e4e6e84a3cbaf7e51e874a00c701dca0150d9
Jorge Obiols <jorge.obiols@gmail.com>
2025-02-05 09:27:55 -0300

    [FIX] repositorios l10n_ar

--- test16e_default/__manifest__.py
+++ test16e_default/__manifest__.py
@@ -33,17 +33,27 @@
     # if Enterprise it installs in a different directory than community
     "odoo-license": "EE",
     # Config to write in odoo.conf
-    "config": [
+    "config_local": [
         "workers = 0",
         "admin_password = admin",
     ],
     "port": "8069",
     "git-repos": [
         "https://github.com/jobiols/cl-test.git -b 16.0e",
+
+        "https://github.com/ingadhoc/account-financial-tools.git sub_l10n-ar/account-financial-tools",
+        "https://github.com/ingadhoc/account-payment.git sub_l10n-ar/account-payment",
+        "https://github.com/ingadhoc/odoo-argentina.git sub_l10n-ar/odoo-argentina",
+        "https://github.com/ingadhoc/argentina-sale.git sub_l10n-ar/argentina-sale",
+        "https://github.com/ingadhoc/account-invoicing.git sub_l10n-ar/account-invoicing",
+        "https://github.com/ingadhoc/odoo-argentina-ee.git sub_l10n-ar/odoo-argentina-ee",
+        "https://github.com/ingadhoc/stock.git sub_l10n-ar/stock",
+        "https://github.com/ingadhoc/aeroo_reports.git sub_l10n-ar/aeroo_reports",
+
     ],
     # list of images to use in the form 'name image-url'
     "docker-images": [
         "odoo jobiols/odoo-ent:16.0e",
-        "postgres postgres:15.1-alpine",
+        "postgres postgres:14.13-alpine",
     ],
 }
